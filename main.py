8dcddbb0d17713abda426d24bbc8fdc714733884
paulocfrossard <paulocorreafrossard@gmail.com>
2023-05-05 23:39:25 -0300

    added 'parameters' and 'minimum limit', optimized code

--- main.py
+++ main.py
@@ -7,24 +7,30 @@ import passwd_strength
 
 while True:
     try:
-        if len(sys.argv) > 1 and sys.argv[1].isdecimal():
-
+        if len(sys.argv) > 4 and sys.argv[1].isdecimal():
             tam = int(sys.argv[1])
         else:
             tam = int(input("Enter size of password: "))
+            if tam < 4:
+                print("Smallest number of accepted digits is 4")
+                continue
+            else:
+                break
         break
+
     except ValueError:
         print("That is not a valid integer number")
 
 list_all_char = passwd_create.generate_ascii_list(33, 127)  # define ascii size
 
 # parameters
-uppercase = True
-lowercase = True
-special = True
-number = True
+uppercase = True  # set as required uppercase
+lowercase = True  # set as required lowercase
+special = True  # set as required special char
+number = True  # set as required number
+parameters = [uppercase, lowercase, number, special]
 
-new_password = passwd_create.generate_password(tam, list_all_char)  # define size of passwd and valid chars
+new_password = passwd_create.generate_password(tam, list_all_char, parameters)  # define size of passwd and valid chars
 
 print("\n[Password]\n[*] %-25s %s\n" % ("Generated password:", new_password))
 
--- passwd_create.py
+++ passwd_create.py
@@ -1,5 +1,5 @@
 import secrets
-
+import string
 
 def generate_ascii_list(x, y):
     ascii_list = []
@@ -15,9 +15,28 @@ def generate_random_char(char_list):
     return random_char
 
 
-def generate_password(tam, char_list):
-    password = ""
-    for i in range(tam):
-        number_pass = generate_random_char(char_list)
-        password += char_list[number_pass]
+def generate_password(tam, char_list, parameters):
+    parameter = False
+    while (parameter != True):
+        decimal_type = upper_type = lower_type = special_type = 0
+        password = ""
+        for i in range(tam):
+            number_pass = generate_random_char(char_list)
+            chargen_password = str(char_list[number_pass])
+            password += chargen_password
+            if chargen_password.isdecimal():
+                decimal_type += 1
+            elif chargen_password.isupper():
+                upper_type += 1
+
+            elif chargen_password.islower():
+                lower_type += 1
+
+            elif chargen_password in string.punctuation:
+                special_type += 1
+
+        if sum([special_type>0, lower_type>0, upper_type>0, decimal_type>0]) == 4:
+            parameter = True
+        else:
+            parameter = False
     return password
